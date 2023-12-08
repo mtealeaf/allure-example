ecf47eae51826f45d579ca39860c5e56232bb9b8
Mollymauk Tealeaf <153263198+mtealeaf@users.noreply.github.com>
2023-12-08 12:22:51 +0300

    Update IssuesWebTest.java

--- src/test/java/io/qameta/allure/IssuesWebTest.java
+++ src/test/java/io/qameta/allure/IssuesWebTest.java
@@ -33,7 +33,7 @@ public class IssuesWebTest {
     @Story("Create new issue")
     @JiraIssues({@JiraIssue("AE-2")})
     @Tags({@Tag("web"), @Tag("critical")})
-    @DisplayName("Creating new issue authorized user")
+    @DisplayName("[11] Creating new issue authorized user")
     public void shouldCreateIssue() {
         steps.openIssuesPage(OWNER, REPO);
         steps.createIssueWithTitle(ISSUE_TITLE);
@@ -46,7 +46,7 @@ public class IssuesWebTest {
     @Story("Create new issue")
     @Tags({@Tag("web"), @Tag("regress")})
     @JiraIssues({@JiraIssue("AE-1")})
-    @DisplayName("Adding note to advertisement")
+    @DisplayName("[12] Adding note to advertisement")
     public void shouldAddLabelToIssue() {
         steps.openIssuesPage(OWNER, REPO);
         steps.createIssueWithTitle(ISSUE_TITLE);
@@ -59,7 +59,7 @@ public class IssuesWebTest {
     @Story("Close existing issue")
     @Tags({@Tag("web"), @Tag("regress")})
     @JiraIssues({@JiraIssue("AE-1")})
-    @DisplayName("Closing new issue for authorized user")
+    @DisplayName("[15] Closing new issue for authorized user")
     public void shouldCloseIssue() {
         steps.openIssuesPage(OWNER, REPO);
         steps.createIssueWithTitle(ISSUE_TITLE);
